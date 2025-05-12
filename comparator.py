43ee171feb6ce11bed8962beeeb93dba9c9d26fd
zeeny <157370421+kzeen@users.noreply.github.com>
2025-05-12 19:43:14 +0300

    Add number of followers and following

--- comparator.py
+++ comparator.py
@@ -19,7 +19,9 @@ for element in following_list:
         unfollowed_list.append(element)
 
 with open("unfollowed_" + datetime.today().strftime("%Y-%m-%d_%H-%M-%S") + ".txt", "w") as f:
-    for unfollowed in unfollowed_list:
+    f.write("Number of followers: " + str(len(followers_list)) + "\n")
+    f.write("Number of following: " + str(len(following_list)) + "\n\n")
+    for unfollowed in unfollowed_list:    
         f.write(unfollowed + "\n\n")
 
 followers_file.close()
